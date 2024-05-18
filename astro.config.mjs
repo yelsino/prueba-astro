3caf43a88b6c40c25b3e87c2dff4b849687a62d7
yelsiño <yelsino321@gmai.com>
2024-05-18 18:34:53 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import db from "@astrojs/db";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), db()],
+  integrations: [mdx(), sitemap(), db(), svelte()],
 });
